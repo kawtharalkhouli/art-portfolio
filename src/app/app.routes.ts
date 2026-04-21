ebe8cee730f9f8ffacac4a28f293e893a808c93f
kawthar alkhouli <kalkhouli@revest.sa>
2026-04-21 22:07:05 +0300

    feat: add HomeComponent with routing, layout, and styles for art portfolio
    - Implemented routing for HomeComponent with lazy loading.
    - Created home.component.html for the main layout including sections for About, Gallery, and Contact.
    - Added home.component.scss for styling the HomeComponent with responsive design.
    - Developed home.component.ts to manage gallery items and scrolling functionality.
    - Included unit tests for HomeComponent.
    - Updated index.html to include Google Fonts and Material Icons.
    - Modified global styles in styles.scss for consistent layout and typography.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: 'home',
+    loadComponent: () =>
+      import('./home/home.component').then((c) => c.HomeComponent),
+    title: 'My Art Portfolio',
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: '**',
+    component: HomeComponent,
+    title: 'Page Not Found',
+  },
+
+];
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,172 @@
+import { Component, ElementRef, signal, ViewChild, WritableSignal } from '@angular/core';
+
+interface GalleryItems {
+  id: number;
+  src: string;
+  name: string;
+  tag: string;
+}
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  @ViewChild('aboutSection') aboutSection!: ElementRef<HTMLElement>;
+  @ViewChild('gallerySection') gallerySection!: ElementRef<HTMLElement>;
+  @ViewChild('contactSection') contactSection!: ElementRef<HTMLElement>;
+  @ViewChild('galleryGrid') galleryGrid!: ElementRef<HTMLElement>;
+  private _resizeObserver!: ResizeObserver;
+  galleryColumns: WritableSignal<number> = signal<number>(5);
+
+  // Gallery Items
+  galleryItems: WritableSignal<GalleryItems[]> = signal<GalleryItems[]>([
+    {
+      id: 1,
+      src: 'images/pink-floral-artwork.jpg',
+      name: 'Pink Floral Artwork',
+      tag: 'Florals',
+    },
+    {
+      id: 2,
+      src: 'images/pink-doodles.jpg',
+      name: 'Pink Doodles',
+      tag: 'Doodles',
+    },
+    {
+      id: 3,
+      src: 'images/doodles.jpg',
+      name: 'Doodles',
+      tag: 'Doodles',
+    },
+    {
+      id: 5,
+      src: 'images/spring-in-pastel-blooms.jpg',
+      name: 'Spring in Pastel Blooms',
+      tag: 'Florals',
+    },
+    {
+      id: 6,
+      src: 'images/spring-time-scribbles.jpg',
+      name: 'Spring Time Scribbles',
+      tag: 'Sketches',
+    },
+    {
+      id: 7,
+      src: 'images/cherry-blossoms.jpg',
+      name: 'Cherry Blossoms',
+      tag: 'Sketches',
+    },
+    {
+      id: 4,
+      src: 'images/bunny-hearts-and-pumpkin-smiles.jpg',
+      name: 'Bunny Hearts and Pumpkin smiles',
+      tag: 'Doodles',
+    },
+    {
+      id: 8,
+      src: 'images/heart-floral-wreath.jpg',
+      name: 'Heart Floral Wreath',
+      tag: 'Water Color',
+    },
+    {
+      id: 9,
+      src: 'images/cute-doodles.jpg',
+      name: 'Cute Doodles',
+      tag: 'Doodles',
+    },
+    {
+      id: 13,
+      src: 'images/walle_artwork.jpg',
+      name: 'WALL-E Artwork',
+      tag: 'Doodles',
+    },
+    {
+      id: 11,
+      src: 'images/floral-patterns.jpg',
+      name: 'Pink Floral Pattern',
+      tag: 'Florals',
+    },
+    {
+      id: 12,
+      src: 'images/lilies-sketch.jpg',
+      name: 'Lilies Sketch',
+      tag: 'Sketches',
+    },
+    {
+      id: 20,
+      src: 'images/whimsical-doodles.jpg',
+      name: 'Whimsical Doodles',
+      tag: 'Doodles',
+    },
+    {
+      id: 21,
+      src: 'images/cute-bunny.jpg',
+      name: 'Cute Bunny Doodles',
+      tag: 'Doodles',
+    },
+    {
+      id: 22,
+      src: 'images/lilies.jpg',
+      name: 'Lilies',
+      tag: 'Florals',
+    },
+  ]);
+
+  ngAfterViewInit(): void {
+    this._reseize();
+  }
+
+  private _reseize(): void {
+    this._resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        this._updateColumns(entry.contentRect.width);
+      }
+    });
+
+    if (this.galleryGrid?.nativeElement) {
+      this._resizeObserver.observe(this.galleryGrid.nativeElement);
+    }
+  }
+
+  private _updateColumns(width: number): void {
+    if (width > 1200) {
+      this.galleryColumns.set(5);
+    } else if (width >= 900) {
+      this.galleryColumns.set(4);
+    } else if (width >= 600) {
+      this.galleryColumns.set(3);
+    } else if (width >= 400) {
+      this.galleryColumns.set(2);
+    } else {
+      this.galleryColumns.set(1);
+    }
+  }
+
+  scrollTo(section: 'about' | 'gallery' | 'contact'): void {
+    const map: Record<string, ElementRef<HTMLElement>> = {
+      about: this.aboutSection,
+      gallery: this.gallerySection,
+      contact: this.contactSection,
+    };
+
+    map[section]?.nativeElement.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+
+  openInstagram() {
+    window.open('https://www.instagram.com/kawtharalkhouli', '_blank');
+  }
+
+  openEmail() {
+    window.location.href = 'mailto:kawthar_alkhouli@outlook.com';
+  }
+
+  ngOnDestroy(): void {
+    this._resizeObserver.disconnect();
+  }
+}
